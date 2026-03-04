0c5841e0d4cf8e857d1020202a47a7a04db2eafc
Ice\UNA <icebenitez@gmail.com>
2026-03-04 13:31:29 +0800

    added vercel to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pastries-shop.vercel.app',
   integrations: [sitemap()],
+
   image: {
     domains: ['images.unsplash.com'],
     remotePatterns: [{
@@ -15,7 +18,10 @@ export default defineConfig({
       hostname: 'images.unsplash.com'
     }],
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
