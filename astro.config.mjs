1698fba6de747d08a8a74af887fd704b31ed5102
Ice\UNA <icebenitez@gmail.com>
2026-03-04 13:47:47 +0800

    fixed site in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pastries-shop.vercel.app',
+  site: 'https://pastry-shop-eta.vercel.app',
   adapter: vercel({
     imageService: true
   }),
