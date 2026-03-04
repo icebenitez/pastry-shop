6d5d7e5c6c0f68ba436c1f5b87f69b1f5bceb99a
Ice\UNA <icebenitez@gmail.com>
2026-03-04 13:40:46 +0800

    cleanups

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pastries-shop.vercel.app',
   adapter: vercel({
-    // imagesConfig: {
-    //   sizes: [320, 640, 1280],
-    // },
     imageService: true
   }),
   output: 'static',
